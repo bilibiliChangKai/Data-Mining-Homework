be58157ed9005efb732272ecf2d47d26c8837d03
Moustache <huzang@mail2.sysu.edu.cn>
2018-06-08 11:27:53 +0800

    update v2.2

--- 1/linear-regression.py
+++ 1/linear-regression.py
@@ -3,59 +3,83 @@ import numpy as np
 import pandas as pd
 from sympy import *
 
-# select a column as y
-def setY(df, columnname):
-    df.insert(len(df.columns), 'y', df[columnname])
-    del df[columnname]
+def normalize(df):
+    newdf = pd.DataFrame()
+    for f in df.columns:
+        newdf[f] = map(lambda x: float(x - df[f].mean()) / (df[f].max() - df[f].min()), df[f])
+    return newdf
 
-# return arg symbols
-def initArg(n, tag='x'):
-    if tag == 'x':
-        string = ""
-    else:
-        string = tag + "0"
-    for i in range(n):
-        string += " " + tag + str(i + 1)
-    return symbols(string)
+class LinearRegression:
+    def __init__(self, df, n):
+        self.df = df
+        self.tas_v0 = zip(initArg(4, 'ta'), np.zeros())
+        self.xs = LinearRegression.initArg(4)
+        self.y = Symbol('y')
+        self.hta_x = LinearRegression.initFunc(tas, xs)
+        self.jta_x = LinearRegression.initSingleJta(hta_x, y)
 
-# return the linear regression
-def initFunc(tas, xs):
-    func = tas[0]
-    for i in range(len(xs)):
-        func = func + tas[i + 1] * xs[i]
-    return func
+    # select a column as y
+    def setY(self, columnname):
+        self.df.insert(len(self.df.columns), 'y', self.df[columnname])
+        del self.df[columnname]
 
-# return the single line J(theta)
-def initSingleJta(func, y):
-    return (func - y) ** 2
+    # return arg symbols
+    @staticmethod
+    def initArg(n, tag='x'):
+        if tag == 'x':
+            string = ""
+        else:
+            string = tag + "0"
+        for i in range(n):
+            string += " " + tag + str(i + 1)
+        return symbols(string)
 
-# calculate the J(theta)'s differential coefficient in tai
-def calcDiffCoe(singleJta, xs, tas_v, y, i, df, alfa=1):
-    m = len(df)
-    n = len(df.columns)
-    lists = [calcSingCoe(singleJta, xs, tas_v, y, i, df.iloc[j]) for j in range(m)]
-    sumvalue = sum(lists)
-    return alfa * sumvalue / (2 * m)
+    # return the linear regression
+    @staticmethod
+    def initFunc(tas, xs):
+        func = tas[0]
+        for i in range(len(xs)):
+            func = func + tas[i + 1] * xs[i]
+        return func
 
-def calcSingCoe(singleJta, xs, tas_v, y, i, arr):
-    Jta_coe = diff(singleJta, tas_v[i][0])
-    # replace arguments
-    for x_v in zip(xs, arr):
-        # print x_v[1]
-        Jta_coe = Jta_coe.subs(x_v[0], x_v[1])
-    for ta_v in tas_v:
-        Jta_coe = Jta_coe.subs(ta_v[0], ta_v[1])
-    Jta_coe = Jta_coe.subs(y, arr[-1])
-    return float(Jta_coe)
+    # return the single line J(theta)
+    @staticmethod
+    def initSingleJta(func, y):
+        return (func - y) ** 2
+
+    # calculate the J(theta)'s differential coefficient in tai
+    def calcDiffCoe(self, singleJta, xs, tas_v, y, ta, df, alfa=1):
+        m = len(df)
+        n = len(df.columns)
+        lists = [calcSingCoe(singleJta, xs, tas_v, y, ta, df.iloc[j]) for j in range(m)]
+        sumvalue = sum(lists)
+        return alfa * sumvalue / (2 * m)
+
+    def calcSingCoe(self, singleJta, xs, tas_v, y, ta, arr):
+        Jta_coe = diff(singleJta, ta)
+        # replace arguments
+        for x_v in zip(xs, arr):
+            # print x_v[1]
+            Jta_coe = Jta_coe.subs(x_v[0], x_v[1])
+        for ta_v in tas_v:
+            Jta_coe = Jta_coe.subs(ta_v[0], ta_v[1])
+        Jta_coe = Jta_coe.subs(y, arr[-1])
+        return float(Jta_coe)
+
+    def cal
+
+    def thetaIteration(tas_v)
+        return map(lambda x : (x[0], x[1] - calcDiffCoe(jta_x, xs, tas_v0, y, x[0], nordf)), tas_v0)
 
 if __name__ == '__main__':
-    tas = initArg(4, 'ta')
-    xs = initArg(4)
-    y = Symbol('y')
-    hta_x = initFunc(tas, xs)
-    jta_x = initSingleJta(hta_x, y)
+
 
     df = pd.read_csv("1.csv")
     setY(df, 'm')
-    for i in range(5):
-        print "Theta" + str(i), "is:", 0 - calcDiffCoe(jta_x, xs, zip(tas, np.zeros(5, float)), y, i, df)
+    nordf = normalize(df)
+
+    print "Q1:"
+    tas_v1 = 
+    print "Theta1 :", tas_v1
+    
+    print "Q2:"
